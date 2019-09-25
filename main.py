bbbfb9443f38d8235113cb22cb8fe504ca46495d
vladv-qa <vladv-qa@users.noreply.github.com>
2019-09-25 16:00:15 +0100

    get temp and wind speed

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ markup_location = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_wi
 user_location = telebot.types.KeyboardButton('Send Location', request_location=True)
 markup_location.add(user_location)
 
+weather_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+current_day_weather = types.KeyboardButton('What the weather?')
+weather_markup.add(current_day_weather)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -18,11 +22,16 @@ def start_message(message):
     bot.send_message(message.chat.id, "To get started I need your location. Please send location",  reply_markup=markup_location)
 
 @bot.message_handler(content_types=['location'])
-def get_user_location(message):
+def get_the_weather(message):
+    user_name = message.chat.first_name
+    bot.send_message(message.chat.id, f"Now {user_name}, choose weather forecast date", reply_markup=weather_markup)
     latitude = message.location.latitude
     longitude = message.location.longitude
-    weather_url = f"api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}"
-
-    return print(weather_url)
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?appid=84ab65202ce25088f419b99eb4d7c1e2&lat={latitude}&lon={longitude}"
+    res = requests.get(weather_url)
+    data = res.json()
+    temperature = data['main']['temp']
+    wind_speed = data['wind']['speed']
+    print(temperature, wind_speed)
 
 bot.polling()
