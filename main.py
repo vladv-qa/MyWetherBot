657678c5d02d5f3557c8bb8729cb41f2a8722b53
vladv-qa <vladv-qa@users.noreply.github.com>
2019-09-26 11:56:32 +0100

    unknown location

--- main.py
+++ main.py
@@ -19,19 +19,32 @@ weather_markup.add(current_day_weather)
 def start_message(message):
     user_name = message.chat.first_name
     bot.send_message(message.chat.id, f"Hello {user_name}! I'm happy that you choose Me!")
-    bot.send_message(message.chat.id, "To get started I need your location. Please send location",  reply_markup=markup_location)
+    bot.send_message(message.chat.id, "To get started I need your location. Please send location",
+                     reply_markup=markup_location)
+
 
 @bot.message_handler(content_types=['location'])
 def get_the_weather(message):
     user_name = message.chat.first_name
-    bot.send_message(message.chat.id, f"Now {user_name}, choose weather forecast date", reply_markup=weather_markup)
     latitude = message.location.latitude
     longitude = message.location.longitude
-    weather_url = f"http://api.openweathermap.org/data/2.5/weather?appid=84ab65202ce25088f419b99eb4d7c1e2&lat={latitude}&lon={longitude}"
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?appid=84ab65202ce25088f419b99eb4d7c1e2&lat={latitude}&lon={longitude}&units=metric"
     res = requests.get(weather_url)
     data = res.json()
-    temperature = data['main']['temp']
-    wind_speed = data['wind']['speed']
-    print(temperature, wind_speed)
+    temperature = data['main']['temp']  # температура
+    description = data['weather'][0]['description']  # опис
+    humidity = data['main']['humidity']  # вологість
+    wind_speed = data['wind']['speed']  # швидкість вітру
+    location = data['name']  # місце знаходження
+    if location == '':
+        location = "Somewhere"
+    bot.send_message(message.chat.id, f"""
+    {user_name}, the forecast in {location} is:
+
+        Temperature: {temperature} C°
+        Description: {description}.
+        Humidity: {humidity} %.
+        Wind speed: {wind_speed} m/s.""", reply_markup=markup_location)
+
 
 bot.polling()
